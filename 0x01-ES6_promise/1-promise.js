2e821629d4ed9d8b6aa5a77126ee9da2aa20146b
ted-thuraa <mutwirited@gmail.com>
2022-12-21 17:23:21 +0300

    u

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,12 @@
 export default function getFullResponseFromAPI(success) {
     return new Promise((resolve, reject) => {
         if (success) {
-            resolve({ status: 200, body: 'success' });
+            resolve({
+                status: 200, 
+                body: 'success', 
+            });
         } else {
-            reject(Error('The fake API is not working currently'));
+            reject(new Error('The fake API is not working currently'));
         }
     });
 }
--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,11 +1,10 @@
-import { uploadPhoto, createUser } from ".utils.js";
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-    const upload = uploadPhoto();
-    const create = createUser();
-    Promise.all([upload, create]).then((result) => {
-        console.log(`${result[0].body} ${result[1].fistName} ${result[1].lastName}`);
-    }).catch(() => {
-        console.log('Signup system offline');
-    });
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((res) => {
+      console.log(`${res[0].body} ${res[1].firstName} ${res[1].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
 }
