b0c810b08303fb28b1b0b34b45b9dc67bd25aed9
ted-thuraa <mutwirited@gmail.com>
2022-12-23 10:00:11 +0000

    up

--- 0x01-ES6_promise/.eslintrc.js
+++ 0x01-ES6_promise/.eslintrc.js
@@ -1,15 +1,28 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es6": true,
+	"jest": true
     },
     "extends": "eslint:recommended",
+    "plugin":"jest/all",
     "overrides": [
+	{
+		"files": "['*.js']",
+		"excludedFiles": 'babel.config.js',
+	}
     ],
     "parserOptions": {
         "ecmaVersion": "latest",
         "sourceType": "module"
     },
     "rules": {
+	    'no-console': 'off',
+	    'no-shadow': 'off',
+	    'no-restricted-syntax': [
+		    'error',
+		    'LabeledStatement',
+		    'WithStatement',
+	    ],
     }
 }
